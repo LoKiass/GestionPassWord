701104dcf439a31f478f00b72b820d84a5dd3be4
LoKias_ <diseursmat@gmail.com>
2024-12-23 21:12:13 +0100

    V.3 : Gestion Password avec retour "*" pour le user, secu. supp pour la touche retour, ajout d'un minimum de charactère pour le mots de passe

--- main.c
+++ main.c
@@ -1,36 +1,39 @@
-// Programme gestion password V1.1
-// Cahier des charges : Ajout de securité au programme (Gestion des touches spéciale et du backspace)
-// +Gestion espace, suprimmer le charactère précedent
+// Programme gestion password V3
+// Cahier des charges : Print une * lors que la personne à bien introduit un charactere, la suprimmer avec un backspace
+// Donner un nombre de charactere minium que le user doit frapper (5)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(void) {
-    int x = 0, TaillePassword = 10, Flagout = 0;
+    int x = 0, TaillePassword = 10, Flagout = 0, MinimumPass = 5;
     char TPassword[TaillePassword+1];
     //Code pour le mots de passe
-    printf("Mots de passe (aucun reflet de la frappe, Taille Max : 10): ");
+    printf("Mots de passe (aucun reflet de la frappe, Taille Max : 10, min 5): ");
     do {
         TPassword[x] = getch();
             if (TPassword[x] == 13) { // Code Enter afin de sortir de programme
-                Flagout = 1;
+                if (x >= MinimumPass) { //Obliger de le verifier sur cette ligne car obliger dans rentre dans la conditions pour ne pas rentre dans celle du else{..}
+                    Flagout = 1;
+                }
             }
             else if (TPassword[x] == 9 || TPassword[x]== 27 || TPassword[x] == 32) { // Code ASCII touche spéciale
                 TPassword[x] = 0;
-                x--;
             }
             else if (TPassword[x] == 8) { // Gestion de la touche retour
-                if(x > 0) {
+                if(x > 0) { // Si x > 0 pour eviter d'effacer le text derrière
+                    printf("\b \b");
                     x--;
                 }
             }
             else {
                 if(x <= TaillePassword - 1) { // Taille maxium du tableau - 1 en comptant le 0
+                    printf("*");
                     x++;
                 }
             }
         }while (Flagout != 1);
-        TPassword[x] = 0;
+        TPassword[x] = 0; // \0 de clôture d'une chaîne de charactère
         printf("\n--> Votre mots de passe est : %s", TPassword); // Verification du mots de passe finale (Sans debugage)
         printf("\n--> Taille du mdp : %d", strlen(TPassword)); // Verification de la taille finale (Sans debugage)
 
